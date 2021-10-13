b71763508458248222d658a862660c0ac4887366
J-R77-stack <johnnyramsay81@gmail.com>
2021-10-13 07:35:11 +0000

    remove whitespace errors

--- run.py
+++ run.py
@@ -1,22 +1,24 @@
-#---------- Global Variables ------------
+# ---------- Global Variables ------------
 
 # Variable containing a list to hold the board game data.
 board = ["-", "-", "-",
-         "-", "-", "-", 
-         "-", "-", "-",]
+         "-", "-", "-",
+         "-", "-", "-"]
 
 # Variable to let the user know if the game is finished
 # set to True, will be False and break out of loop when game is over.
-game_still_running = True         
+game_still_running = True
 
-# Variable to inform the user who the winner is. stays none when there is a draw
-# and changes to X or O when there is a winner
+# Variable to inform the user who the winner is. stays none
+# when there is a draw and changes to X or O when there is a winner.
+
 winner = None
 
 # Variable to inform the users who the current player is. Starts with player X
 current_player = "X"
 
-#-------- Functions -------------------------
+# -------- Functions -------------------------
+
 
 def show_board():
     """
@@ -30,36 +32,37 @@ def show_board():
     print("|" + board[6] + "|" + board[7] + "|" + board[8] + "|")
     print("\n")
 
+
 def play_the_game():
     """
     Function to play a game of Noughts and crosses.
     """
-
-    show_board() 
+    show_board()
 
     while game_still_running:
         """
-        While loop to loop through which players turn it is, 
+        While loop to loop through which players turn it is,
         to see if the game is over and if X or O is the winner
-        or there is a tie and print the info to the console 
+        or there is a tie and print the info to the console
         and to change the player on each round.
         """
-        game_turn(current_player) 
+        game_turn(current_player)
 
         see_if_game_is_over()
 
-        change_player()  
+        change_player()
 
     if winner == "X" or winner == "O":
         print(winner + " is the winner")
     elif winner == None:
-        print("Its a draw")        
+        print("Its a draw")
+
 
 def game_turn(player):
     """
-    Function to deal with the turn of each player. 
+    Function to deal with the turn of each player.
     They can enter a number to choose a position on the board.
-    """ 
+    """
     print(player + " Go!")
     number = input("Please choose a number from 1 - 9: ")
 
@@ -67,44 +70,46 @@ def game_turn(player):
     While loop to loop for correct input and spot on board is open.
 
     """
-    allowed = False  
+    allowed = False
     while not allowed:
 
       """
-      While loop to keep asking user to enter a number from 1-9 
+      While loop to keep asking user to enter a number from 1-9
       and to display Not allowed if incorrect input typed in
-      """  
+      """
 
       while number not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
           number = input("Not Allowed! Please choose a number from 1 - 9: ")
       """
-      This will get the correct position in the board list. 
-      As board is a string convert to integer. position is 1 - 9  
+      This will get the correct position in the board list.
+      As board is a string convert to integer. position is 1 - 9
       but elements in board Array are 0 - 8
       so position 1 = 0 so need to subtract 1 from number.
       """
-      number = int(number) - 1
+    number = int(number) - 1
 
-      if board[number] == "-":
-          allowed = True
-      else:    
-          print("That position is taken! Go again please")
-
+    if board[number] == "-":
+        allowed = True
+    else:
+        print("That position is taken! Go again please")
 
     board[number] = player
+
     show_board()
 
-def see_if_game_is_over(): 
+
+def see_if_game_is_over():
     """
     Fuction to see if game can end through win or draw.
     """
     see_if_winner()
-    see_if_draw()  
+    see_if_draw()
 
-def see_if_winner(): 
+
+def see_if_winner():
     """
     Fuction to check to see if there is a winner.
-    """ 
+    """
     global winner
     row_winner = check_rows()
 
@@ -119,14 +124,15 @@ def see_if_winner():
     elif column_winner:
         winner = column_winner
     else:
-        winner = None            
+        winner = None
     return
+
 
 def check_rows():
     """
-    Function with if else statement to check the rows for a win seeing if 
-    all rows have the same value and are not empty. 
-    If any rows do match it will flag the while loop 
+    Function with if else statement to check the rows for a win seeing if
+    all rows have the same value and are not empty.
+    If any rows do match it will flag the while loop
     that the game is over and return the winner X or O
     or return None/False if there is no winner and jumps out of loop.
     """
@@ -144,15 +150,14 @@ def check_rows():
     elif row_3:
         return board[6]
     else:
-        return None   
+        return None
 
-        
 
 def check_diagonals():
     """
-    Function to check the diagonals for a win seeing if 
-    all diagonals have the same value and are not empty. 
-    If any diagonals do match it will flag the while loop 
+    Function to check the diagonals for a win seeing if
+    all diagonals have the same value and are not empty.
+    If any diagonals do match it will flag the while loop
     that the game is over and return the winner X or O
     or return None/False if there is no winner and jumps out of loop.
     """
@@ -167,12 +172,13 @@ def check_diagonals():
     elif diagonal_2:
         return board[2]
     else:
-        return None 
+        return None
+
 
 def check_columns():
     """
-    Function to check the columns for a win seeing if 
-    all columns have the same value and are not empty. 
+    Function to check the columns for a win seeing if
+    all columns have the same value and are not empty.
     If any columns do match it will flag the while loop
      that the game is over and return the winner X or O
     or return None/False if there is no winner and jumps out of loop.
@@ -191,8 +197,8 @@ def check_columns():
     elif column_3:
         return board[2]
     else:
-        return None  
-                
+        return None
+
 
 def see_if_draw():
     """
@@ -202,7 +208,8 @@ def see_if_draw():
     global game_still_running
     if "-" not in board:
         game_still_running = False
-    return   
+    return
+
 
 def change_player():
     """
@@ -212,7 +219,7 @@ def change_player():
     if current_player == "X":
         current_player = "O"
     elif current_player == "O":
-        current_player = "X"    
-    return           
+        current_player = "X"
+    return
 
-play_the_game()    
+play_the_game()
